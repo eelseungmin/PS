80952fd1f86a4eaa3b6b27ae0c659f6ccd19d79c
devberry <90882909+devberry@users.noreply.github.com>
2022-09-19 15:18:54 +0900

    [Bronze V] Title: A+B, Time: 124 ms, Memory: 14184 KB -BaekjoonHub

---
+++ 백준/Bronze/1000. A＋B/A＋B.java
@@ -0,0 +1,18 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        System.out.print(n + k);
+
+        br.close();
+    }
+}
