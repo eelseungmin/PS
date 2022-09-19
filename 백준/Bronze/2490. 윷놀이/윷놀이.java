7ac65a494d7221ff6cfeb9de29bf76a805ad21ae
devberry <90882909+devberry@users.noreply.github.com>
2022-09-19 20:55:41 +0900

    [Bronze III] Title: 윷놀이, Time: 120 ms, Memory: 14228 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.java
+++ 백준/Bronze/2490. 윷놀이/윷놀이.java
@@ -6,15 +6,10 @@ public class Main {
     public static void main(String[] args) throws IOException {
         // write your code here
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st;
-
-        int cnt = 0;
-        char res = ' ';
 
         for (int j = 0; j < 3; j++) {
-            cnt = 0;
-            st = new StringTokenizer(br.readLine());
+            int cnt = 0;
+            StringTokenizer st = new StringTokenizer(br.readLine());
 
             for (int i = 0; i < 4; i++)
                 if (Integer.parseInt(st.nextToken()) == 0) {
@@ -23,29 +18,21 @@ public class Main {
 
             switch (cnt) {
                 case 0:
-                    res = 'E';
+                    System.out.println("E");
                     break;
-
                 case 1:
-                    res = 'A';
+                    System.out.println("A");
                     break;
-
                 case 2:
-                    res = 'B';
+                    System.out.println("B");
                     break;
-
                 case 3:
-                    res = 'C';
+                    System.out.println("C");
                     break;
-
                 case 4:
-                    res = 'D';
+                    System.out.println("D");
                     break;
             }
-
-            bw.write(res + "\n");
         }
-
-        bw.flush();
     }
 }
