78ba4bb44bbdf759850602ea0a55e00849e06c3d
devberry <90882909+devberry@users.noreply.github.com>
2022-09-19 19:11:00 +0900

    [Bronze V] Title: 고양이, Time: 116 ms, Memory: 13904 KB -BaekjoonHub

---
+++ 백준/Bronze/10171. 고양이/고양이.java
@@ -0,0 +1,13 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        System.out.print("\\    /\\\n" +
+                " )  ( ')\n" +
+                "(  /  )\n" +
+                " \\(__)|");
+    }
+}
