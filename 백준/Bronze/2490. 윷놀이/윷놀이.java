646fc58f4f5d4c1c8dad99024702dd876e686aae
devberry <90882909+devberry@users.noreply.github.com>
2022-09-19 20:45:01 +0900

    [Bronze III] Title: 윷놀이, Time: 148 ms, Memory: 15888 KB -BaekjoonHub

--- 백준/Bronze/2490. 윷놀이/윷놀이.java
+++ 백준/Bronze/2490. 윷놀이/윷놀이.java
@@ -7,104 +7,45 @@ public class Main {
         // write your code here
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-        StringTokenizer st = new StringTokenizer(br.readLine());
-        StringTokenizer st2 = new StringTokenizer(br.readLine());
-        StringTokenizer st3 = new StringTokenizer(br.readLine());
+        StringTokenizer st;
 
         int cnt = 0;
         char res = ' ';
 
-        for (int i = 0; i < 4; i++) {
-            if (Integer.parseInt(st.nextToken()) == 0) {
-                cnt++;
-            }
-        }
+        for (int j = 0; j < 3; j++) {
+            cnt = 0;
+            st = new StringTokenizer(br.readLine());
 
-        switch (cnt) {
-            case 0:
-                res = 'E';
-                break;
+            for (int i = 0; i < 4; i++)
+                if (Integer.parseInt(st.nextToken()) == 0) {
+                    cnt++;
+                }
+
+            switch (cnt) {
+                case 0:
+                    res = 'E';
+                    break;
 
                 case 1:
-                res = 'A';
-                break;
+                    res = 'A';
+                    break;
 
                 case 2:
-                res = 'B';
-                break;
+                    res = 'B';
+                    break;
 
                 case 3:
-                res = 'C';
-                break;
+                    res = 'C';
+                    break;
 
                 case 4:
-                res = 'D';
-                break;
-        }
-
-        bw.write(res + "\n");
-        cnt = 0;
-
-        for (int i = 0; i < 4; i++) {
-            if (Integer.parseInt(st2.nextToken()) == 0) {
-                cnt++;
+                    res = 'D';
+                    break;
             }
+
+            bw.write(res + "\n");
         }
 
-        switch (cnt) {
-            case 0:
-                res = 'E';
-                break;
-
-            case 1:
-                res = 'A';
-                break;
-
-            case 2:
-                res = 'B';
-                break;
-
-            case 3:
-                res = 'C';
-                break;
-
-            case 4:
-                res = 'D';
-                break;
-        }
-
-        bw.write(res + "\n");
-        cnt = 0;
-
-        for (int i = 0; i < 4; i++) {
-            if (Integer.parseInt(st3.nextToken()) == 0) {
-                cnt++;
-            }
-        }
-
-        switch (cnt) {
-            case 0:
-                res = 'E';
-                break;
-
-            case 1:
-                res = 'A';
-                break;
-
-            case 2:
-                res = 'B';
-                break;
-
-            case 3:
-                res = 'C';
-                break;
-
-            case 4:
-                res = 'D';
-                break;
-        }
-
-        bw.write(res + "\n");
         bw.flush();
     }
 }
