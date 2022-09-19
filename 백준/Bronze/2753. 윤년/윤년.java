117ea2315f0a8917fe9f80e7662e21c946a2a727
devberry <90882909+devberry@users.noreply.github.com>
2022-09-19 19:43:57 +0900

    [Bronze V] Title: 윤년, Time: 124 ms, Memory: 14276 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,21 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+        int a = Integer.parseInt(br.readLine());
+
+        if (a % 4 == 0 && a % 100 != 0 || a % 400 == 0) {
+            bw.write(1+"");
+        } else {
+            bw.write(0+"");
+        }
+
+        bw.flush();
+    }
+}
