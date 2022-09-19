735ab4ed240b8d92a2d3fbd39110af916395cc98
devberry <90882909+devberry@users.noreply.github.com>
2022-09-19 19:36:50 +0900

    [Bronze IV] Title: 세수정렬, Time: 140 ms, Memory: 15912 KB -BaekjoonHub

---
+++ 백준/Bronze/2752. 세수정렬/세수정렬.java
@@ -0,0 +1,25 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+
+    public static void main(String[] args) throws IOException {
+	// write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int[] arr = new int[3];
+
+        for (int i = 0; i < 3; i++) {
+            arr[i] = Integer.parseInt(st.nextToken());
+        }
+
+        Arrays.sort(arr);
+
+        for (int i = 0; i < 3; i++) {
+            bw.write(arr[i] + " ");
+        }
+        bw.flush();
+    }
+}
