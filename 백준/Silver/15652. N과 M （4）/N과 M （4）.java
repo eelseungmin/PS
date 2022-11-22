573e12b3549b3ad17747defbf94716d4ee80771c
dltmdalssssss <90882909+dltmdalssssss@users.noreply.github.com>
2022-11-22 20:19:36 +0900

    [Silver III] Title: N과 M (4), Time: 204 ms, Memory: 19468 KB -BaekjoonHub

---
+++ 백준/Silver/15652. N과 M （4）/N과 M （4）.java
@@ -0,0 +1,43 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    /*
+    1초, 512MB
+     */
+    static int n, m;
+    static int[] arr = new int[9];
+    static boolean[] isUsed = new boolean[9];
+    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+	// write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        func(0);
+        bw.flush();
+        bw.close();
+    }
+
+    public static void func(int num) throws Exception {
+        if (num == m) {
+            for (int i = 0; i < m; i++) {
+                bw.write(arr[i] + " ");
+            }
+            bw.write("\n");
+
+            return;
+        }
+
+        for (int i = 1; i <= n; i++) {
+            if (num != 0 && arr[num - 1] > i) {
+                continue;
+            }
+            arr[num] = i;
+            func(num + 1);
+        }
+    }
+}
