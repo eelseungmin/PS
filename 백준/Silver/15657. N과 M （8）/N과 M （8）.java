ea2b1ff710bd5562837c8bf186ec6609bb3c7792
dltmdalssssss <90882909+dltmdalssssss@users.noreply.github.com>
2022-11-23 21:12:11 +0900

    [Silver III] Title: N과 M (8), Time: 216 ms, Memory: 18128 KB -BaekjoonHub

---
+++ 백준/Silver/15657. N과 M （8）/N과 M （8）.java
@@ -0,0 +1,50 @@
+import java.io.*;
+import java.util.*;
+
+public class Main {
+    /*
+    1초, 512MB
+     */
+    static int n, m;
+    static int[] arr;
+    static int[] nums;
+    static BufferedWriter bw =  new BufferedWriter(new OutputStreamWriter(System.out));
+
+    public static void main(String[] args) throws Exception {
+	// write your code here
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        arr = new int[n];
+        nums = new int[n];
+        st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(nums);
+
+        func(0);
+        bw.flush();
+        bw.close();
+    }
+
+    public static void func(int num) throws Exception {
+        if (num == m) {
+            for (int i = 0; i < m; i++) {
+                bw.write(arr[i] + " ");
+            }
+            bw.write("\n");
+
+            return;
+        }
+
+        for (int i = 0; i < n; i++) {
+            if (num != 0 && arr[num - 1] > nums[i]) {
+                continue;
+            }
+            arr[num] = nums[i];
+            func(num + 1);
+        }
+    }
+}
