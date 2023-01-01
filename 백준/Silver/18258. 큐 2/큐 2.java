604a1f106ead94dc7ef755ddcc4985ee21af53d9
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-01-01 19:47:20 +0900

    [Silver IV] Title: 큐 2, Time: 1452 ms, Memory: 350592 KB -BaekjoonHub

--- 백준/Silver/18258. 큐 2/큐 2.java
+++ 백준/Silver/18258. 큐 2/큐 2.java
@@ -1,63 +1,58 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.StringTokenizer;
 
 public class Main {
-    /*
-
-     */
     public static void main(String[] args) throws Exception {
         // write your code here
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
-
         LinkedList<Integer> q = new LinkedList<>();
+        StringBuilder sb = new StringBuilder();
 
         int n = Integer.parseInt(br.readLine());
-        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < n; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
-            String temp = st.nextToken();
 
-            switch (temp) {
+            switch (st.nextToken()) {
                 case "push":
                     q.offer(Integer.parseInt(st.nextToken()));
                     break;
-
                 case "pop":
-                    if (q.isEmpty()) {
-                        sb.append("-1\n");
+                    if (!q.isEmpty()) {
+                        sb.append(q.poll());
                     } else {
-                        sb.append(q.poll() + "\n");
+                        sb.append(-1);
                     }
+                    sb.append("\n");
                     break;
-
                 case "size":
                     sb.append(q.size() + "\n");
                     break;
-
                 case "empty":
-                    if (q.isEmpty()) {
-                        sb.append("1\n");
+                    if (!q.isEmpty()) {
+                        sb.append(0);
                     } else {
-                        sb.append("0\n");
+                        sb.append(1);
                     }
+                    sb.append("\n");
                     break;
-
                 case "front":
-                    if (q.isEmpty()) {
-                        sb.append("-1\n");
+                    if (!q.isEmpty()) {
+                        sb.append(q.peek());
                     } else {
-                        sb.append(q.getFirst() + "\n");
+                        sb.append(-1);
                     }
+                    sb.append("\n");
                     break;
-
                 case "back":
-                    if (q.isEmpty()) {
-                        sb.append("-1\n");
+                    if (!q.isEmpty()) {
+                        sb.append(q.getLast());
                     } else {
-                        sb.append(q.getLast() + "\n");
+                        sb.append(-1);
                     }
+                    sb.append("\n");
                     break;
             }
         }
