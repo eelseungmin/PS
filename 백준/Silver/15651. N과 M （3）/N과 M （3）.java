48c0bd45d0d6671c736d62b400d6fa113a122822
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-01-01 22:16:43 +0900

    [Silver III] Title: N과 M (3), Time: 444 ms, Memory: 67852 KB -BaekjoonHub

--- 백준/Silver/15651. N과 M （3）/N과 M （3）.java
+++ 백준/Silver/15651. N과 M （3）/N과 M （3）.java
@@ -1,40 +1,37 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.util.StringTokenizer;
 
 public class Main {
-    /*
-    1초, 512MB
-     */
-    static int n, m;
     static int[] arr = new int[8];
-    static boolean[] isUsed = new boolean[8];
-    static BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+    static int n, m;
+    static StringBuilder sb = new StringBuilder();
 
     public static void main(String[] args) throws Exception {
-	// write your code here
+        // write your code here
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
+
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
 
-        func(0);
-        bw.flush();
-        bw.close();
+        func(1);
+
+        System.out.println(sb);
     }
 
-    public static void func(int num) throws Exception {
-        if (num == m) {
-            for (int i = 0; i < m; i++) {
-                bw.write(arr[i] + " ");
+    public static void func(int k) { // k번째 수를 선택
+        if (k == m + 1) {
+            for (int i = 1; i < m + 1; i++) {
+                sb.append(arr[i]).append(" ");
             }
-            bw.write("\n");
-
+            sb.append("\n");
             return;
         }
 
-        for (int i = 1; i <= n; i++) {
-                arr[num] = i;
-                func(num + 1);
+        for (int i = 1; i < n + 1; i++) {
+            arr[k] = i;
+            func(k + 1);
         }
     }
 }
