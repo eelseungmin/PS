706ee3ee4de5eb6198cb066713262c98ff4cdf78
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-01-03 16:35:42 +0900

    [Silver III] Title: 스택 수열, Time: 344 ms, Memory: 27212 KB -BaekjoonHub

--- 백준/Silver/1874. 스택 수열/README.md
+++ 백준/Silver/1874. 스택 수열/README.md
@@ -1,10 +1,10 @@
-# [Silver II] 스택 수열 - 1874 
+# [Silver III] 스택 수열 - 1874 
 
 [문제 링크](https://www.acmicpc.net/problem/1874) 
 
 ### 성능 요약
 
-메모리: 48900 KB, 시간: 1504 ms
+메모리: 27212 KB, 시간: 344 ms
 
 ### 분류
 
--- 백준/Silver/1874. 스택 수열/스택 수열.java
+++ 백준/Silver/1874. 스택 수열/스택 수열.java
@@ -1,59 +1,36 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
 
 public class Main {
-    /*
-    수열 4 3 6 8 7 5 2 1
-    stack 1 2 5 6
-    1. 숫자를 하나 입력 받는다.
-    2. 해당 숫자까지 스택에 push한다.
-    3. 해당 숫자가 수열에 들어가도록 pop한다.
-    4. 다음 숫자를 입력 받는다.
-    5. 해당 숫자가 더 크면 다시 해당 숫자까지 스택에 push한다.
-    6. pop한다.
-     */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         // write your code here
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
+        StringBuilder sb = new StringBuilder();
+        Stack<Integer> stack = new Stack<>();
         int n = Integer.parseInt(br.readLine());
 
-        Stack<Integer> stack = new Stack<>();
-        List<Character> list = new ArrayList<>();
-        int last = 0; // 마지막으로 스택에 push한 숫자
-        int cnt = 0; // 모든 과정을 마친 후 cnt != n -> NO
-        for (int i = 1; i <= n; i++) {
-            int temp = Integer.parseInt(br.readLine());
+        int last = 0; // last push
+        for (int i = 0; i < n; i++) {
+            int num = Integer.parseInt(br.readLine());
 
-            if (!stack.isEmpty()) {
-                if (stack.peek() == temp) {
-                    stack.pop();
-                    cnt++;
-                    list.add('-');
-                    continue;
+            if (last < num) {
+                while (last != num) {
+                    stack.push(++last);
+                    sb.append('+').append('\n');
                 }
             }
 
-            if (last < temp) {
-                for (int j = last + 1; j <= temp; j++) {
-                    stack.push(j);
-                    list.add('+');
-                }
-                last = temp;
+            if (stack.peek() != num) {
+                System.out.println("NO");
+                return;
             }
 
-            if (stack.pop() == temp) { // 스택에서 꺼낸 숫자가 입력 받은 숫자와 일치
-                cnt++;
-            }
-            list.add('-');
+            stack.pop();
+            sb.append('-').append('\n');
         }
 
-        if (cnt == n) {
-            for (char ch : list) {
-                System.out.println(ch);
-            }
-        } else {
-            System.out.println("NO");
-        }
+        System.out.println(sb);
     }
 }
