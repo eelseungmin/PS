7ac0a85d140429eb9c1663ed6f9c6f0e66d1faf2
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-01-05 17:48:01 +0900

    [Gold V] Title: 탑, Time: 864 ms, Memory: 85264 KB -BaekjoonHub

--- 백준/Gold/2493. 탑/탑.java
+++ 백준/Gold/2493. 탑/탑.java
@@ -1,5 +1,8 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.Stack;
+import java.util.StringTokenizer;
 
 class Tower {
     int num;
@@ -13,47 +16,43 @@ class Tower {
 
 public class Main {
     /*
-    1.5초 128MB
-    4.5~7.5억번 3천만개
-
-    1. 스택이 비어있으면 0 출력, 현재 탑 push
-    2. 스택이 비어있지 않으면 다음 케이스 검사
-    3-1. 현재 탑의 높이보다 peek의 높이가 높다면 해당 탑의 번호 출력 후 현재 탑 push
-    3-2. 현재 탑의 높이보다 peek의 높이가 낮다면 pop한 후 2번으로 돌아간다.
+    탑 높이 push
+    6 9 5 7 4 3 8 6
+    6
+    9
+    모든 탑을 돌면서 다 push를 하되
+    이번 탑의 높이 > peek일 동안 계속 pop
+    이번 탑의 높이 <= peek일 경우 peek의 높이 출력
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         // write your code here
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-        StringBuilder sb = new StringBuilder();
-
         int n = Integer.parseInt(br.readLine());
         StringTokenizer st = new StringTokenizer(br.readLine());
-
         Stack<Tower> stack = new Stack<>();
+        StringBuilder sb = new StringBuilder();
 
         for (int i = 0; i < n; i++) {
-            int height = Integer.parseInt(st.nextToken());
+            Tower cur = new Tower(i + 1, Integer.parseInt(st.nextToken()));
 
-            if (stack.isEmpty()) { // 가장 왼쪽 탑
-                sb.append(0 + " ");
-                stack.push(new Tower(i + 1, height));
-            } else {
-                while (true) { // 현재 탑보다 높은 탑을 찾을 때까지
-                    if (stack.isEmpty()) { // 현재 탑보다 높은 탑이 더 이상 존재하지 않으면
-                        sb.append(0 + " ");
-                        stack.push(new Tower(i + 1, height));
-                        break;
-                    }
-
-                    if (stack.peek().height > height) {
-                        sb.append(stack.peek().num + " ");
-                        stack.push(new Tower(i + 1, height));
-                        break;
-                    } else {
+            if (!stack.isEmpty()) {
+                while (!stack.isEmpty()) {
+                    if (cur.height > stack.peek().height) {
                         stack.pop();
+                    } else {
+                        break;
                     }
                 }
+
+                if (!stack.isEmpty()) {
+                    sb.append(stack.peek().num).append(" ");
+                } else {
+                    sb.append(0).append(" ");
+                }
+            } else {
+                sb.append(0).append(" ");
             }
+            stack.push(cur);
         }
 
         System.out.println(sb);
