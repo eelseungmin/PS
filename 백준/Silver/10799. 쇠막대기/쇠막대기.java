a141f705af0d326073b5abbdbf763a97826c1221
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-01-05 12:29:37 +0900

    [Silver III] Title: 쇠막대기, Time: 180 ms, Memory: 16256 KB -BaekjoonHub

--- 백준/Silver/10799. 쇠막대기/쇠막대기.java
+++ 백준/Silver/10799. 쇠막대기/쇠막대기.java
@@ -3,37 +3,33 @@ import java.util.*;
 
 public class Main {
     /*
-    (.)((((.)(.))((.))(.)))((.))
-    (는 push
-    )는 이전 문자가 (면 레이저, )면 막대기
-    레이저가 나가는 순간의 스택의 길이만큼 더하고
-    막대기가 끝나면 해당 막대기는 1개만큼만 더 나오니까 +1
+    ( -> push
+    ) -> pop 이후 ans += stack.size()(바로 왼쪽 문자가 '('일 경우에만)
+    바로 왼쪽 문자가 '('이 아니라면 ans += 1
      */
-    public static void main(String[] args) throws Exception {
+    public static void main(String[] args) throws IOException {
         // write your code here
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-//        StringBuilder sb = new StringBuilder();
+        String input = br.readLine();
+        Stack<Character> stack = new Stack<>();
 
-        String str = br.readLine();
-        Stack<Character> s = new Stack<>();
-        int cnt = 0;
+        int ans = 0;
+        for (int i = 0; i < input.length(); i++) {
+            char cur = input.charAt(i);
 
-        for (int i = 0; i < str.length(); i++) {
-            char ch = str.charAt(i);
-
-            if (ch == '(') { // (
-                s.push(ch);
-            } else { // )
-                if (str.charAt(i - 1) == '(') { // 레이저
-                    s.pop();
-                    cnt += s.size();
-                } else { // 막대기 끝
-                    s.pop();
-                    cnt++;
+            if (cur == '(') {
+                stack.push(cur);
+            } else {
+                stack.pop();
+                if (input.charAt(i - 1) == '(') {
+                    ans += stack.size();
+                } else {
+                    ans += 1;
                 }
             }
         }
 
-        System.out.println(cnt);
+        System.out.println(ans);
     }
+
 }
