4504a9c0830c27f73f7d3856a419f559c7ac036f
dltmdalssssss <dltmdalssssss0520@gmail.com>
2023-02-12 16:20:52 +0900

    [Gold V] Title: 토마토, Time: 628 ms, Memory: 123432 KB -BaekjoonHub

--- 백준/Gold/7576. 토마토/토마토.java
+++ 백준/Gold/7576. 토마토/토마토.java
@@ -1,9 +1,12 @@
-import java.io.*;
-import java.util.*;
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 
 class Node {
-    int x;
-    int y;
+    int x, y;
 
     public Node(int x, int y) {
         this.x = x;
@@ -13,37 +16,32 @@ class Node {
 
 public class Main {
     /*
-    정수 1은 익은 토마토, 정수 0은 익지 않은 토마토, 정수 -1은 토마토가 들어있지 않은 칸
-    모든 익지 않은 토마토로부터 익은 토마토까지의 거리 중 최단을 찾는 것
-    -> 익은 토마토만 bfs 시작점으로 취급
+    O(NM)
      */
-    static int[][] tomato;
+    static int n, m;
+    static int[][] map;
     static int[][] dist;
     static int[] dx = {-1, 1, 0, 0};
     static int[] dy = {0, 0, -1, 1};
-    static int max = 0;
-    static Queue<Node> q;
+    static Queue<Node> q = new LinkedList<>();
 
-    public static void main(String[] args) throws Exception {
-        // write your code here
+    public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
         StringTokenizer st = new StringTokenizer(br.readLine());
-
-        int m = Integer.parseInt(st.nextToken());
-        int n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+        n = Integer.parseInt(st.nextToken());
+        map = new int[n][m];
         dist = new int[n][m];
-        tomato = new int[n][m];
-        q = new LinkedList<>();
 
-        // 입력 받으면서 익은 토마토만 큐에 집어넣기
         for (int i = 0; i < n; i++) {
             st = new StringTokenizer(br.readLine());
+
             for (int j = 0; j < m; j++) {
-                tomato[i][j] = Integer.parseInt(st.nextToken());
-                if (tomato[i][j] == 1) {
+                map[i][j] = Integer.parseInt(st.nextToken());
+                if (map[i][j] == 1) {
                     q.offer(new Node(i, j));
                 }
-                if (tomato[i][j] == 0) {
+                if (map[i][j] == 0) {
                     dist[i][j] = -1;
                 }
             }
@@ -51,20 +49,21 @@ public class Main {
 
         bfs();
 
+        int ans = 0;
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if (dist[i][j] == -1) { // bfs 돌렸는데도 익지 않은 토마토가 있으면 -1 출력
+                if (dist[i][j] == -1) {
                     System.out.println(-1);
                     return;
                 }
-                max = Math.max(max, dist[i][j]);
+                ans = Math.max(ans, dist[i][j]);
             }
         }
 
-        System.out.println(max);
+        System.out.println(ans);
     }
 
-    public static void bfs() {
+    static void bfs() {
         while (!q.isEmpty()) {
             Node cur = q.poll();
 
@@ -72,11 +71,11 @@ public class Main {
                 int nx = cur.x + dx[i];
                 int ny = cur.y + dy[i];
 
-                if (nx < 0 || nx >= tomato.length || ny < 0 || ny >= tomato[0].length) {
+                if (nx < 0 || nx >= n || ny < 0 || ny >= m) {
                     continue;
                 }
 
-                if (dist[nx][ny] >= 0) {
+                if (dist[nx][ny] != -1) {
                     continue;
                 }
 
