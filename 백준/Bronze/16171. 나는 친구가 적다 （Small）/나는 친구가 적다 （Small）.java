fa1bb443485270793bd06cbdc7a996c3a951b570
Seungmin Lee <berrypicker777@gmail.com>
2023-10-12 20:14:31 +0900

    [Bronze II] Title: 나는 친구가 적다 (Small), Time: 196 ms, Memory: 17600 KB -BaekjoonHub

---
+++ 백준/Bronze/16171. 나는 친구가 적다 （Small）/나는 친구가 적다 （Small）.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Main {
+
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+
+        String s = sc.next();
+        String k = sc.next();
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < s.length(); i++) {
+            char ch = s.charAt(i);
+            if (Character.isAlphabetic(ch)) {
+                sb.append(ch);
+            }
+        }
+
+        if (sb.toString().contains(k)) {
+            System.out.println(1);
+        } else {
+            System.out.println(0);
+        }
+    }
+}
