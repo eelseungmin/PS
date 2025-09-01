e4f50e4e2f3e4d0a29e06c87a224ee13e4e96442
eelseungmin <eelseungmin@gmail.com>
2025-09-01 13:35:29 +0900

    [level 2] Title: 가장 큰 수, Time: 2877.26 ms, Memory: 396 MB -BaekjoonHub

--- 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
+++ 프로그래머스/2/42746. 가장 큰 수/가장 큰 수.java
@@ -2,25 +2,21 @@ import java.util.*;
 
 class Solution {
     public String solution(int[] numbers) {
-        // int 상태에서 내림차순 정렬? -> 10, 6, 2 -> 1062 -> X
-        // String 상태에서 내림차순 정렬? -> 마찬가지
-        // 10, 6 -> 106, 6, 10 -> 610 같은 경우를 모두 대응하기 위해 둘의 순서를 바꿔가며 더해서 나온 문자열을 비교해서 내림차순 정렬한다.
-        String[] nums = new String[numbers.length];
+        // 사전순 내림차순 정렬
+        String[] numStr = new String[numbers.length];
         for (int i = 0; i < numbers.length; i++) {
-            nums[i] = String.valueOf(numbers[i]);
+            numStr[i] = String.valueOf(numbers[i]);
         }
         
-        Arrays.sort(nums, (s1, s2) -> {
-           return (s2 + s1).compareTo(s1 + s2);
-        });
+        Arrays.sort(numStr, (n1, n2) -> (n2 + n1).compareTo(n1 + n2));
         
-        if (nums[0].equals("0")) {
+        if (numStr[0].equals("0")) {
             return "0";
         }
         
         String ans = "";
-        for (String n : nums) {
-            ans += n;
+        for (String num : numStr) {
+            ans += num;
         }
         
         return ans;
